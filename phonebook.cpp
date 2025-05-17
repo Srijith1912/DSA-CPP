4e51ca3e8561710db5c5d5e7f6160253ef34c054
Srijith Mulupuri <165108998+Srijith1912@users.noreply.github.com>
2025-05-17 12:45:52 -0400

    Update phonebook.cpp

--- phonebook.cpp
+++ phonebook.cpp
@@ -7,11 +7,25 @@ using namespace std;
 
 int main(){
 
-unordered_map<string, string> phonebook;
+  string name, phone_number;
+  char c;
+  unordered_map<string, string> phonebook;
 
-phonebook["Sathwik"] = "000-000-0001";
-phonebook["Shahid"] = "000-000-0002";
-cout << Sathwik's phone number << phonebook["Sathwik"] << endl;
+  do{
+    cout << "Enter name: ";
+    getline(cin, name);
+    cout << "Enter phone number: ";
+    getline(cin, phone_number);
+    phonebook[name] = phone_number;
+    cout << "Add more contacts? (y/n)" << endl;
+    cin >> c;
+    cin.ignore();
+  } while (c == 'y' || c == 'Y');
+
+  cout << "\nPhonebook: " << endl;
+  for (const auto& pair : phonebook) {
+    cout << pair.first << ": " << pair.second << endl;
+  }
+
 return 0;
-
 }
