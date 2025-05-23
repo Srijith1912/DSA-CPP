5f531925c6d9d95425546ca03ca54a187d4650b7
Srijith Mulupuri <165108998+Srijith1912@users.noreply.github.com>
2025-05-22 23:42:46 -0400

    Update 0 - Phonebook.cpp

--- 0 - Phonebook.cpp
+++ 0 - Phonebook.cpp
@@ -1,3 +1,10 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 #include <iostream>
 #include <stdio.h>
 #include <vector>
@@ -35,64 +42,95 @@ int main()
     string name, number;
     vector<pair<string, string>> phonebook;
     bool flag = true;
+    char c;
 
     do{
         
-    cout << "Hi! What would you like to do?\n1. Add a contact(1) \n2. Look up a contact(2) \n3. Display phonebook(3) \n4. Quit(4) \n" << endl;
+    cout << "Hi! What would you like to do?\n1. Add a contact(1) \n2. Look up a contact(2) \n3. Display phonebook(3) \n4. Delete a contact(4) \n5. Clear phonebook(5) \n6. Quit(6) \n" << endl;
     cin >> choice;
     cin.ignore();
 
-    switch(choice){
-    
-        case 1: {
-            cout << "Enter name of the contact." << endl;
-            getline(cin, name);
-            cout << "Enter number of the contact." << endl;
-            getline(cin, number);
-            phonebook.push_back({name, number});
-            cout << "The contact: " << name << "- " << number << " has been added to the phonebook." << endl;
+        switch(choice){
         
-            break;
-        }
-        case 2: {
-            cout << "Enter name of the contact you want to find" << endl;
-            getline(cin, name);
+            case 1: {
+                cout << "\nEnter name of the contact." << endl;
+                getline(cin, name);
+                cout << "Enter number of the contact." << endl;
+                getline(cin, number);
+                phonebook.push_back({name, number});
+                cout << "\nThe contact: " << name << " - " << number << " has been added to the phonebook.\n" << endl;
             
-            vector<pair<string, string>> sorted_phonebook = phonebook;
-            sort(sorted_phonebook.begin(), sorted_phonebook.end());
-            
-            int pos = binarySearch(sorted_phonebook, name);
-            
-            if (pos != -1){
-                cout << "Found contact: " << sorted_phonebook[pos].first << ": " << sorted_phonebook[pos].second << endl;
+                break;
             }
-            else{
-                cout << "Contact not found." << endl;
-            }
-            
-            break;
-        } 
-        case 3: {
-        
-            if (phonebook.empty()){
-                cout << "Phonebook is empty." << endl;
-            }
-            else{
-                for (const auto& pair: phonebook){
-                    cout << pair.first << ": " << pair.second << endl;
+            case 2: {
+                cout << "\nEnter name of the contact you want to find" << endl;
+                getline(cin, name);
+                
+                vector<pair<string, string>> sorted_phonebook = phonebook;
+                sort(sorted_phonebook.begin(), sorted_phonebook.end());
+                
+                int pos = binarySearch(sorted_phonebook, name);
+                
+                if (pos != -1){
+                    cout << "\nFound contact: " << sorted_phonebook[pos].first << ": " << sorted_phonebook[pos].second << "\n"<< endl;
                 }
-            }
-            break;
-        }
-        case 4: {
+                else{
+                    cout << "\nContact not found.\n" << endl;
+                }
+                break;
+            } 
+            case 3: {
             
-            flag = false;
-            break;
-        }    
-        default: {
-            cout << "Select a choice shown above: 1, 2, 3 or 4" << endl;
-        }    
-    }
+                if (phonebook.empty()){
+                    cout << "\nPhonebook is empty." << endl;
+                }
+                else{
+                    for (const auto& pair: phonebook){
+                        cout << pair.first << ": " << pair.second << endl;
+                    }
+                }
+                break;
+            }
+            
+            case 4: {
+                
+                cout << "Enter the name of the contact you want to delete: " << endl;
+                getline(cin, name);
+                
+                int p = 0;
+                
+                while (phonebook[p].first != name){
+                    p++;
+                }
+                phonebook.erase(phonebook.begin() + p);
+                cout << "Contact has been deleted." << endl;
+                
+                break;
+            }
+            
+            case 5: {
+                
+                cout << "This is going to erase all your phonebook data. Are you absolutely sure? (y/n)" << endl;
+                cin >> c;
+                
+                if (c == 'y' || c == 'Y'){
+                    
+                    phonebook.clear();
+                    cout << "Phonebook has been cleared." << endl;
+                } 
+                break;
+            }
+            
+            case 6: {
+                
+                flag = false;
+                break;
+            }
+            
+            default: {
+                cout << "Select a choice shown above: 1, 2, 3, 4, 5 or 6" << endl;
+            }    
+        }
         
     } while (flag);
     
